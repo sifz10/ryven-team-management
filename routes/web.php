2f7b52d9f17578ebf306cd4f82955432427a1e6c
Sifat Kazi <75115364+sifz10@users.noreply.github.com>
2025-10-14 22:25:39 +0600

    Add artisan command execution route for authenticated users

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/artisan/{command}', function (Request $request) {
+        Artisan::call($request->command);
+        return 'Executed artisan command boss!';
+    });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
