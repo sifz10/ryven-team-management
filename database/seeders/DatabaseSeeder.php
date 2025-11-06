08d300ffe632a8ca4ac3ab476641c7ac870ab541
Sifat Kazi <75115364+sifz10@users.noreply.github.com>
2025-11-06 21:28:37 +0600

    feat: Add seeder calls for permissions, roles, and employees in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(AdminUserSeeder::class);
+        $this->call(PermissionSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(EmployeeSeeder::class);
     }
 }
